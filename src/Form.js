0c0ffbf04ae52d88294c55be2e7d60d1741b7732
jpcastil <40906436+jpcastil@users.noreply.github.com>
2021-01-08 21:32:01 -0800

    Finished!

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-function Form() {   
+function Form(props) {   
     function handleChange(event) {
         const { name, value } = event.target;
         if (name === "job")
@@ -12,7 +12,12 @@ function Form() {
               {name: value, job: person['job']}
            );   
       }
-      
+
+      function submitForm() {
+        props.handleSubmit(person);
+        setPerson({name: '', job: ''});
+      }
+
    const [person, setPerson] = useState(
       {  
          name: '',
@@ -36,6 +41,7 @@ function Form() {
             id="job"
             value={person.job}
             onChange={handleChange} />
+        <input type="button" value="Submit" onClick={submitForm} />
         </form>
     ); 
 
